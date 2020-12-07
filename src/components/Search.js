05206be68654cfa488b09bdff1e4e736f1aa3a46
MCauz <michaelhcausey@gmail.com>
2020-12-06 20:18:58 -0800

    set name added to search

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,32 +2,34 @@ import React, { useState } from 'react'
 
 function Search() {
 
-    const [searchTerm, setSearchTerm] = useState("")
-
-
+    const [cardName, setCardName] = useState("")
+    const [cardSet, setCardSet] = useState("")
+    // const [searchParams, setSearchParams] = useState("")
 
     function fetchSearch(searchTerm) {
-        fetch(`https://api.magicthegathering.io/v1/cards?name=${searchTerm}`)
+        fetch(`https://api.magicthegathering.io/v1/cards?${cardName}`)
         .then(resp => resp.json())
         .then(results => console.log(results))
     }
 
-    function handleSearchTerm(e) {
-        setSearchTerm(e.target.value)
+    function handleCardName(e) {
+        setCardName(e.target.value)
+    }
+    
+    function handleCardSet(e) {
+        setCardSet(e.target.value)
     }
     
     function searchCards(e) {
         e.preventDefault();
-        fetchSearch(searchTerm)
     }
-    
-    
     
     return(
         <div>
             Search Component
             <form>
-                <input onChange={handleSearchTerm} type='text' name='card name' placeholder='Card Name' />
+                <input onChange={handleCardName} type='text' name='card name' placeholder='Card Name' /><br></br>
+                <input onChange={handleCardSet} type='text' name='set' placeholder='Card Set' /><br></br>
                 <button onClick={searchCards} type='submit'>Search</button>
             </form>
         </div>
