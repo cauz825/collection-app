0de7b42177f0a63e845094868f4e6f5aee786c18
MCauz <michaelhcausey@gmail.com>
2020-12-13 23:00:49 -0800

    Navbar component added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Inventory from './components/Inventory'
 import Search from './components/Search'
+import Navbar from './components/Navbar';
 import './App.css';
 
 function App() {
   return (
     <div>
+      <Navbar />
       <h1>Collection App</h1>
       <Search /><br></br>
       {/* <Inventory /> */}
