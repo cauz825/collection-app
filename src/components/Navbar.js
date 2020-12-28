3571a55832e8db427dac838c111bc189b28f1bad
MCauz <michaelhcausey@gmail.com>
2020-12-27 18:13:37 -0800

    navbar links added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,12 @@ import React from 'react'
 function Navbar() {
     return(
         <div>
-            
+            <ul id="nav">
+                <li><a>Home</a></li>
+                <li><a>Collection</a></li>
+                <li><a>Wishlist</a></li>
+                <li><a>Search</a></li>
+              </ul>
         </div>
     )
 }
