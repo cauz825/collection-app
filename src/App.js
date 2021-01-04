cd70d4d27625879901fe36a551273f2c5ef5c345
MCauz <michaelhcausey@gmail.com>
2021-01-03 20:12:24 -0800

    router on hold

--- src/App.js
+++ src/App.js
@@ -2,17 +2,16 @@ import React from 'react';
 import Search from './components/Search'
 import Navbar from './components/Navbar';
 import './App.css';
-import {BrowserRouter, Route} from 'react-router-dom'
+// import {BrowserRouter, Route} from 'react-router-dom'
 
 
 function App() {
   return (
-      <BrowserRouter>
-        <div>
-          <Navbar />
-            <Route path="/search" component={Search} />
-        </div>
-      </BrowserRouter>
+    <div>
+      <Navbar />
+      <Search />
+    </div>
+      
   );
 }
 
