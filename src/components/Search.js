2f83ce1b1c171b1cc9922b5adb9f5c2072f43fbe
MCauz <michaelhcausey@gmail.com>
2021-03-26 15:45:55 -0700

    search form building

--- src/components/Search.js
+++ src/components/Search.js
@@ -8,7 +8,6 @@ function Search() {
     const [cardName, setCardName] = useState("")
     const [cardSet, setCardSet] = useState("")
     const [cardColor, setCardColor] = useState("")
-    const [cardType, setCardType] = useState("")
     const [searchUrl, setSearchUrl] = useState(baseUrl)
     const [searchResults, setSearchResults] = useState([])
 
@@ -64,7 +63,6 @@ function Search() {
                 <input onChange={handleCardName} type='text' name='card name' placeholder='Card Name' /><br></br>
                 <input onChange={handleCardSet} type='text' name='set' placeholder='Card Set' /><br></br>
                 <input onChange={handleCardColor} type='text' name='color' placeholder='Card Color' /><br></br>
-                <input onChange={handleCardType} type='text' name='type' placeholder='Card Type' /><br></br>
                 <input type='text' name='cmc' placeholder='Coverted Mana Cost' /><br></br>
                 <button onClick={searchCards} type='submit'>Search</button>
             </form>
--- src/components/Search2.js
+++ src/components/Search2.js
@@ -1,33 +1,44 @@
 import React, { useEffect, useState } from 'react'
-import Results from './Results'
+// import Results from './Results'
 
 function Search() {
 
     let baseUrl = 'https://api.scryfall.com'
 
-    const [cardName, setCardName] = useState('')
-    const [searchResults, setSearchResults] = useState([])
+    // const [cardName, setCardName] = useState('')
+    // const [searchResults, setSearchResults] = useState([])
 
-    function fetchSearch() {
-        fetch(searchUrl)
-        .then(resp => resp.json())
-        .then(results => setSearchResults(results))
-    }
+    // function fetchSearch() {
+    //     fetch(searchUrl)
+    //     .then(resp => resp.json())
+    //     .then(results => setSearchResults(results))
+    // }
 
-    function handleCardName(e) {
-        setCardName(e.target.value);
-        handleSearchUrl();
-    }
+    // function handleCardName(e) {
+    //     setCardName(e.target.value);
+    //     handleSearchUrl();
+    // }
 
-    function handleSearchUrl() {
+    // function handleSearchUrl() {
 
-    }
+    // }
 
     return(
         <div>
             Search Component
             <form>
-                <input onChange={handleCardName} type='text' name='card name' placeholder='Card Name' /><br></br>
+                <input type='text' name='card name' placeholder='Card Name' /><br></br>
+                <input type='text' name='set' placeholder='Card Set' /><br></br>
+                <select>
+                    <option value='white'>White</option>
+                    <option value='blue'>Blue</option>
+                    <option value='black'>Black</option>
+                    <option value='red'>Red</option>
+                    <option value='green'>Green</option>
+                </select><br></br>
+                <input type='number' name='cmc' placeholder='Converted Mana Cost' /><br></br>
+                <input type='text' name='rule text' placeholder='Rule Text' /><br></br>
+                <button type='submit'>Search</button>
             </form>
         </div>
     )
