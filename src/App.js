0bdb8a0aa3b1cb04b8b26050c5e92fc4f40a844b
MCauz <michaelhcausey@gmail.com>
2021-03-28 21:57:12 -0700

    cardsearch page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 // import Search from './components/Search'
 import Navbar from './components/Navbar';
-import Search from './components/Search2'
+import Search from './components/Search2';
+import CardSearch from './components/CardSearch';
 import './App.css';
 import {BrowserRouter, Route} from 'react-router-dom'
 
---
+++ src/components/CardSearch.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CardSearch() {
+    return(
+        <div>
+            
+        </div>
+    )
+}
+
+export default CardSearch;
