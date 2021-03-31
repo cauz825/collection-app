0cac3ebff36f5fb8078b7e90c8aabad22f35f8a6
MCauz <michaelhcausey@gmail.com>
2021-03-31 14:34:55 -0700

    collection component added

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import React from 'react';
-// import Search from './components/Search'
+import Search from './components/Search'
 import Navbar from './components/Navbar';
-import Search from './components/Search2';
+// import Search from './components/Search2';
 import CardSearch from './components/CardSearch';
+import Collection from './components/Collection'
 import './App.css';
-import {BrowserRouter, Route} from 'react-router-dom'
+// import {BrowserRouter, Route} from 'react-router-dom'
 
 
 function App() {
   return (
-    <BrowserRouter>
       <div>
         <Navbar />
         <Search />
         <CardSearch />
+        <Collection />
       </div>
-    </BrowserRouter>
       
   );
 }
--- src/components/Search.js
+++ src/components/Search.js
@@ -31,11 +31,6 @@ function Search() {
         setCardColor(e.target.value);
         handleSearchUrl()
     }
-
-    function handleCardType(e) {
-        setCardType(e.target.value);
-        handleSearchUrl()
-    }
     
     function searchCards(e) {
         e.preventDefault();
@@ -63,7 +58,6 @@ function Search() {
                 <input onChange={handleCardName} type='text' name='card name' placeholder='Card Name' /><br></br>
                 <input onChange={handleCardSet} type='text' name='set' placeholder='Card Set' /><br></br>
                 <input onChange={handleCardColor} type='text' name='color' placeholder='Card Color' /><br></br>
-                <input type='text' name='cmc' placeholder='Coverted Mana Cost' /><br></br>
                 <button onClick={searchCards} type='submit'>Search</button>
             </form>
             <Results searchResults={searchResults} />
